ce665695589390968d9e7f8bfc27085bbc29b84d
Abdul Hamid Suleman <rockleesan03@gmail.com>
2022-10-03 15:27:11 -0700

    changed

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - prints possible combinations of three digits
+ *
+ * Description: The program prints possible combiantions
+ * of three different digits
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i,j,k;
+
+	i = 48;
+	j = 48;
+	k = 48;
+
+	while  (i<58)
+	{
+		j = i + 1;
+		while (j<58)
+		{
+			k = j + 1;
+			while (k<58)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i != 55)
+				{
+					putchar(44);
+					putchar(32);
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
