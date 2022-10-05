ce1f2c06452c2d240a129821daf1b94430776032
Abdul Hamid Suleman <rockleesan03@gmail.com>
2022-10-05 06:39:58 -0700

    deleted

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,7 +10,7 @@
 
 void times_table(void)
 {
-	int row, column, product, tens, ones;
+	int row, column, tens, ones, x, products;
 
 	for (row = 0; row <= 9; row++)
 	{
@@ -19,26 +19,26 @@ void times_table(void)
 			product = row * column;
 			tens = product / 10;
 			ones = product % 10;
-
-			if (column == 0)
-			{
-				_putchar('0');
-			}
-			else if (product < 10)
+			if (product > 9)
 			{
 				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
+				_putchar(32);
+				_putchar(tens + '0');
 				_putchar(ones + '0');
+			}
+			else if (column == 0)
+			{
+				_putchar(product + '0');
 			}
 			else
 			{
 				_putchar(',');
-				_putchar(' ');
-				_putchar(tens + '0');
-				_putchar(ones + '0');
+				_putchar(32);
+				_putchar(32);
+				_putchar(product + '0');
 			}
 		}
 		_putchar('\n');
 	}
+	return 0;
 }
